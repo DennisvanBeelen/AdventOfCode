cda62f2d37c016311e2a94c593bf776d35f89531
Dennis van Beelen <dvbeelen@gmail.com>
2020-11-12 21:44:40 +0100

    Initial commit

---
+++ TwentyNineteen/DayOneChallengeOne.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TwentyNineteen
+{
+    public class DayOneChallengeOne
+    {
+        public void CalculateFuelNeededSimple()
+        {
+            string[] masses = System.IO.File.ReadAllLines(
+                "D:\\SoftwareEngineering\\Software Projects\\AdventOfCode\\TwentyNineteen\\assets\\spaceCraftMass.txt");
+            int total = 0;
+
+            foreach (var mass in masses)
+            {
+                total += GetFuelFromMass(Convert.ToInt32(mass));
+            }
+
+            Console.WriteLine(total);
+        }
+
+        public void CalculateFuelNeededWithItsOwnFuel()
+        {
+            string[] masses = System.IO.File.ReadAllLines(
+                "D:\\SoftwareEngineering\\Software Projects\\AdventOfCode\\TwentyNineteen\\assets\\spaceCraftMass.txt");
+            int total = 0;
+
+            foreach (var mass in masses)
+            {
+                var subTotal = GetFuelFromMass(Convert.ToInt32(mass));
+                var tempFuel = subTotal;
+                bool moreFuelNeeded = true;
+
+                while (moreFuelNeeded)
+                {
+                    tempFuel = GetFuelFromMass(tempFuel);
+                    if (tempFuel <= 0) moreFuelNeeded = false;
+                    else subTotal += tempFuel;
+                }
+                total += subTotal;
+            }
+
+            Console.WriteLine(total);
+        }
+
+        public int GetFuelFromMass(int mass)
+        {
+            return (mass / 3) - 2;
+        }
+    }
+}
---
+++ TwentyNineteen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TwentyNineteen
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var challenge = new DayOneChallengeOne();
+            
+            challenge.CalculateFuelNeededWithItsOwnFuel();
+
+        }
+    }
+}
